05007888bd56a0f461a119d3cbe31274c1ee5764
Timothy1007 <a0975336738@gmail.com>
2026-09-18 00:06:37 +0800

    Export isolated V2 core modules

---
+++ v2/src/game/index.js
@@ -0,0 +1,7 @@
+export * from './constants.js';
+export * from './state.js';
+export * from './missions.js';
+export * from './death.js';
+export * from './corruption.js';
+export * from './judgments.js';
+export * from './round.js';
