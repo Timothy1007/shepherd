f942609e6d81aec14be8524ebc768dbf35f02cc6
Timothy1007 <a0975336738@gmail.com>
2026-09-18 00:32:05 +0800

    Export V2 card corruption data

--- v2/src/game/index.js
+++ v2/src/game/index.js
@@ -3,5 +3,6 @@ export * from './state.js';
 export * from './missions.js';
 export * from './death.js';
 export * from './corruption.js';
+export * from './card-corruption.js';
 export * from './judgments.js';
 export * from './round.js';
